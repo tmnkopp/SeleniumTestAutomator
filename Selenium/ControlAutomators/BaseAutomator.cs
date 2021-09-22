541bf7fb22c1c99dd52f94bfac90cb1bb0a27f65
Tim Kopp <timkopp@gmail.com>
2021-09-22 14:50:17 -0400

    udpate serilog in driver service

--- Selenium/ControlAutomators/BaseAutomator.cs
+++ Selenium/ControlAutomators/BaseAutomator.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,8 +21,18 @@ namespace CyberScope.Tests.Selenium
     public interface IAutomator
     {
         string PK_FORM { get; set; }
-        string ContainerSelector { get; set; }
+        string ContainerSelector { get; set; } 
         void Automate(ChromeDriver driver);
+    }
+    public interface ISessionContext
+    {
+        ILogger Logger{ get; set; }
+        ChromeDriver Driver { get; set; } 
+    }
+    public class SessionContext : ISessionContext
+    {
+        public ILogger Logger { get ; set; }
+        public ChromeDriver Driver { get; set; }
     }
     public class AutomatorEventArgs : EventArgs
     {
@@ -40,7 +51,7 @@ namespace CyberScope.Tests.Selenium
         protected IList<IWebElement> inputs;
         protected IWebElement input; 
         private string _PK_FORM; 
-        public string PK_FORM { get => _PK_FORM; set => _PK_FORM = value;  }
+        public string PK_FORM { get => _PK_FORM; set => _PK_FORM = value;  } 
         public string DataCall {
             get {
                 var lst = PK_FORM?.Split('-');
