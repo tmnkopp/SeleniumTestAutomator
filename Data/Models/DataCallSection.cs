eb33f206cbb7a682a937af71af8887a12c9d019a
Tim Kopp <timkopp@gmail.com>
2022-04-25 09:08:47 -0400

    refactor repo

---
+++ Data/Models/DataCallSection.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic; 
+namespace CyberScope.Automator
+{ 
+    public class DataCallSection
+    {
+        public DataCallSection(){ 
+        }   
+		public string SectionText { get; set; } 
+		public string URL { get; set; } 
+    } 
+}
+    
---
+++ Data/Models/InitializeAttempt.cs
@@ -0,0 +1,18 @@
+﻿namespace CyberScope.Automator
+{
+    public class InitializeAttempt
+    { 
+        #region CTOR 
+        public InitializeAttempt()
+        {
+        } 
+        #endregion 
+        
+        #region PROPS 
+        public string Tab { get; set; } 
+        public string Section { get; set; } 
+        public object[] GetAsRow => new object[] { Tab, Section }; 
+        #endregion
+
+    }
+}
